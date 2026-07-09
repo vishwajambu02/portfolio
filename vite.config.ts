0a29c3aca29be29dbd77ab5fcbc141a0c663ff15
Vishwa Jambu <vishwaj@myyahoo.com>
2026-07-09 23:52:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   esbuild: {
     loader: 'tsx',
     include: /src\/.*\.tsx?$/,
   },
   base: '/',
   build: {
-    cssCodeSplit: false,
     outDir: 'dist',
   }
 });
